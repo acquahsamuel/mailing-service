ab51993be34d2512fdaa79fae07e2e62903a80ae
acquahsamuel <acquahsamuel1996@gmail.com>
2021-07-03 10:37:47 +0000

    package.json

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
-# Angular Demo Project
+# Project Demo
 
-[Heroku](http://www.protractortest.org/).
+### Structure 
+
+
+| Components        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| Dashboard     | right-aligned | $1600 |
+| Navbar     | centered      |   $12 |
+| Shared     | are neat      |    $1 |
+
+src (Component)
+login
+dashboard 
+navbar
+table
+
+
+images 
+Google Authentication 
+
+Dashboard image
+
+
+
+[Heroku](link).
 
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
-# angular-demo
--- server/server.js
+++ server/server.js
@@ -1,42 +1,57 @@
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+// const path = require("path");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
 
+// const userRoutes = require("./routes/user");
 
-const userRoutes = require("./routes/user");
+// const app = express();
 
-const app = express();
+// mongoose
+//   .connect(
+//     ""
+//   )
+//   .then(() => {
+//     console.log("Connected to database!");
+//   })
+//   .catch(() => {
+//     console.log("Connection failed!");
+//   });
 
-mongoose
-  .connect(
-    ""
-  )
-  .then(() => {
-    console.log("Connected to database!");
-  })
-  .catch(() => {
-    console.log("Connection failed!");
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use("/images", express.static(path.join("backend/images")));
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+//   );
+//   next();
+// });
+
+// app.use("/api/posts", postsRoutes);
+// app.use("/api/user", userRoutes);
+
+// module.exports = app;
+
+let faker = require("faker");
+let database = { products: [] };
+
+for (let i = 1; i <= 300; i++) {
+  database.products.push({
+    id: i,
+    product: faker.commerce.product(),
+    name: faker.commerce.productName(),
+    description: faker.lorem.sentences(),
+    price: faker.commerce.price(),
+    imageUrl: "https://source.unsplash.com/1600x900/?product",
   });
+}
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
-
-app.use("/api/posts", postsRoutes);
-app.use("/api/user", userRoutes);
-
-module.exports = app;
+console.log(JSON.stringify(database));
