d6a59c05eef1f3fdc5d8b625e448780bf3dce7bd
acquahsamuel <iamsamuelacquah@gmail.com>
2021-11-01 18:00:19 +0000

    rounting module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AdminComponent } from "./admin/admin.component";
+import { AccountComponent } from "./admin/account/account.component";
+import { CertificatesComponent } from "./admin/certificates/certificates.component";
+import { DashboardComponent } from "./admin/dashboard/dashboard.component";
+import { PurchasesComponent } from "./admin/purchases/purchases.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
@@ -10,9 +13,13 @@ const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
-  { path: "admin", component: AdminComponent }
-];
 
+
+  { path: "dashboard/courses", component: DashboardComponent },
+  { path: "dashboard/certificates", component: CertificatesComponent },
+  { path: "dashboard/purchases", component: PurchasesComponent },
+  { path: "dashboard/account", component: AccountComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
