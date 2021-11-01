2b5d39fd3bb168e0bbe191f3d95bd81733548a6a
acquahsamuel <iamsamuelacquah@gmail.com>
2021-11-01 18:01:33 +0000

    app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,9 +14,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AdminComponent } from './admin/admin.component';
 import { RegisterComponent } from './register/register.component';
-
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { CertificatesComponent } from './admin/certificates/certificates.component';
+import { CoursesComponent } from './admin/courses/courses.component';
+import { PurchasesComponent } from './admin/purchases/purchases.component';
+import { AccountComponent } from './admin/account/account.component';
 
 @NgModule({
   declarations: [
@@ -24,8 +27,12 @@ import { RegisterComponent } from './register/register.component';
     HomeComponent,
     LoginComponent,
     NavbarComponent,
-    AdminComponent,
-    RegisterComponent
+    RegisterComponent,
+    DashboardComponent,
+    CertificatesComponent,
+    CoursesComponent,
+    PurchasesComponent,
+    AccountComponent,
   ],
   imports: [
     BrowserModule,
