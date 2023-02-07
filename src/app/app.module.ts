8ad6c9cf8e3718331fe35a7bb21c3a47758f083f
acquahsamuel <iamsamuelacquah@gmail.com>
2023-02-07 18:26:24 +0000

    WIP : email-client build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,44 +8,17 @@ import { AuthService } from './shared/services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NotificationService } from './shared/services/notification.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CardComponent } from './modules/card/card.component';
-import { CardsComponent } from './modules/cards/cards.component';
-import { ButtonComponent } from './modules/button/button.component';
-import { TypeCheckComponent } from './modules/type-check/type-check.component';
-import { SecuritySessionComponent } from './modules/security-session/security-session.component';
-import { WikipediaComponent } from './modules/wikipedia/wikipedia.component';
-import { SearchBarComponent } from './modules/search-bar/search-bar.component';
-import { PageListComponent } from './modules/page-list/page-list.component';
-import { CustomStepperComponent } from './modules/custom-stepper/custom-stepper.component';
-import { PlaceholderComponent } from './modules/placeholder/placeholder.component';
-import { FormHandlingComponent } from './modules/form-handling/form-handling.component';
-import { FormsModule } from '@angular/forms';
-import { CreditCheckComponent } from './modules/credit-check/credit-check.component';
-import { CoverterPipe } from './modules/credit-check/coverter.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    CardsComponent,
-    ButtonComponent,
-    TypeCheckComponent,
-    SecuritySessionComponent,
-    WikipediaComponent,
-    SearchBarComponent,
-    PageListComponent,
-    CustomStepperComponent,
-    PlaceholderComponent,
-    FormHandlingComponent,
-    CreditCheckComponent,
-    CoverterPipe
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    FormsModule,
     HttpClientModule
   ],
   providers: [
--- src/app/modules/credit-check/coverter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CoverterPipe } from './coverter.pipe';
-
-describe('CoverterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CoverterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
