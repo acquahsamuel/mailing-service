667b15378baca8699ab084397e10e1e827673020
acquahsamuel <iamsamuelacquah@gmail.com>
2023-02-07 13:47:10 +0000

    WIP : checkcard checker | pipes

--- src/app/modules/credit-check/coverter.pipe.ts
+++ src/app/modules/credit-check/coverter.pipe.ts
@@ -5,8 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CoverterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(value: any, targetUnit: string): any {
+    if (!value) {
+      return '';
+    }
 
+
+    switch (targetUnit) {
+      case 'km':
+        return value * 1.600943;
+      case 'm':
+        return value * 1.60943 * 10000;
+      case 'cm':
+        return value * 1.60943 * 1000 * 1000;
+    }
+  }
 }
