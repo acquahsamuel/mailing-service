d602e0c611731694c51d40361273b2f5c4b1c2e8
acquahsamuel <iamsamuelacquah@gmail.com>
2023-02-07 18:31:01 +0000

    WIP : email-client build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,18 @@ import { AuthService } from './shared/services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NotificationService } from './shared/services/notification.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import { EmailCreateComponent } from './modules/inbox/email-create/email-create.component';
+import { EmailIndexComponent } from './modules/inbox/email-index/email-index.component';
+import { EmailShowComponent } from './modules/inbox/email-show/email-show.component';
+import { EmailReplyComponent } from './modules/inbox/email-reply/email-reply.component';
+ 
 @NgModule({
   declarations: [
     AppComponent,
-   
+    EmailCreateComponent,
+    EmailIndexComponent,
+    EmailShowComponent,
+    EmailReplyComponent
   ],
   imports: [
     BrowserModule,
